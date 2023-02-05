b48fcc6c4b98424b72ad658e8e6e627714d351ec
Bun-letsgogo <bun.coding@gmail.com>
2023-02-05 14:52:19 +0800

    setup post message

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,34 +1,34 @@
 import styled from "styled-components";
 import React, { useEffect, useState } from "react";
-import PropTypes from "prop-types"
+import PropTypes from "prop-types";
 
 const API_ENDPOINT = "https://student-json-api.lidemy.me/comments";
 
 const Page = styled.div`
   width: 360px;
   margin: 0 auto;
-`
+`;
 
 const Title = styled.h1`
   color: #333;
-`
+`;
 
 const MessageForm = styled.form`
   margin-top: 16px;
-`
+`;
 
 const MessageTextArea = styled.textarea`
   display: block;
   width: 100%;
-`
+`;
 
 const SubmitButton = styled.button`
   margin-top: 8px;
-`
+`;
 
 const MessageList = styled.div`
   margin-top: 16px;
-`
+`;
 
 const MessageContainer = styled.div`
   border: 1px solid black;
@@ -38,7 +38,7 @@ const MessageContainer = styled.div`
   &:not(first-child) {
     margin-top: 8px;
   }
-`
+`;
 
 const MessageHead = styled.div`
   display: flex;
@@ -46,25 +46,39 @@ const MessageHead = styled.div`
   justify-content: space-between;
   border-bottom: 1px solid rgba(0, 0, 0, 0.3)
   padding-bottom: 4px;
-`
+`;
 
 const MessageAuther = styled.div`
   color: #333;
   font-size: 14px;
-`
+`;
 
-const MessageTime = styled.div``
+const MessageTime = styled.div``;
 const MessageBody = styled.div`
   margin-top: 16px;
   font-size: 16px;
-`
+`;
 
 const ErrorMessage = styled.p`
   margin-top: 16px;
   color: red;
 `;
 
-function Message({auther, time, children}) {
+const Loading = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: rgba(0, 0, 0, 0.3);
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 30px;
+`
+
+function Message({ auther, time, children }) {
   return (
     <MessageContainer>
       <MessageHead>
@@ -73,38 +87,93 @@ function Message({auther, time, children}) {
       </MessageHead>
       <MessageBody>{children}</MessageBody>
     </MessageContainer>
-  )
+  );
 }
 
 Message.propTypes = {
   auther: PropTypes.string,
   time: PropTypes.string,
-  children: PropTypes.node
-}
+  children: PropTypes.node,
+};
 
 function App() {
+  const [messages, setMessages] = useState(null);
+  const [messageApiError, setMessageApiError] = useState(null);
+  const [value, setValue] = useState("");
+  const [postMessageError, setPostMessageError] = useState("")
+  const [isLoadingPostMessage, setIsLoadingPostMessage] = useState(false)
 
-  const [messages, setMessages] = useState(null)
-  const [apiError, setApiError] = useState(null)
+  const fetchMessage = () => {
+    fetch(API_ENDPOINT)
+      .then((res) => res.json())
+      .then((data) => {
+        setMessages(data.reverse());
+      })
+      .catch((err) => {
+        setMessageApiError(err.message);
+      });
+  };
+
+  const handleTextareaChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (isLoadingPostMessage) return;
+    setIsLoadingPostMessage(true)
+
+    fetch(API_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        nickname: "bun",
+        body: value,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoadingPostMessage(false);
+        if (data.ok === 0) {
+          setPostMessageError(data.message);
+          return;
+        }
+        fetchMessage();
+        setValue("");
+      })
+      .catch((err) => {
+        setIsLoadingPostMessage(false);
+        setPostMessageError(err.message);
+      });
+  };
+
+  const handleTextareaFocus = () => {
+    setPostMessageError(null)
+  }
 
   // App component render完後去 fetch api，因為只有畫面 render完後 fetch一次所以要放[]
   useEffect(() => {
-    fetch(API_ENDPOINT)
-    .then((res) => res.json())
-    .then((data) => {
-      setMessages(data)
-    })
-    .catch(err => {
-      setApiError(err.message)
-    })
-  }, [])
+    fetchMessage()
+  }, []);
 
   return (
     <Page>
+      {isLoadingPostMessage && <Loading>Loading...</Loading>}
       <Title>留言板</Title>
-      <MessageForm>
-        <MessageTextArea rows={10} />
+      <MessageForm onSubmit={handleFormSubmit}>
+        <MessageTextArea
+          rows={10}
+          value={value}
+          onChange={handleTextareaChange}
+          onFocus={handleTextareaFocus}
+        />
         <SubmitButton>送出留言</SubmitButton>
+        {postMessageError && (
+          <ErrorMessage>{postMessageError}</ErrorMessage>
+        )}
       </MessageForm>
       <MessageList>
         {messages &&
@@ -120,8 +189,12 @@ function App() {
             );
           })}
       </MessageList>
-      {apiError && <ErrorMessage>Something went wrong. {apiError.toString()}</ErrorMessage>}
-      {messages && messages.length === 0 && <p>No Message.</p>} 
+      {messageApiError && (
+        <ErrorMessage>
+          Something went wrong. {messageApiError.toString()}
+        </ErrorMessage>
+      )}
+      {messages && messages.length === 0 && <p>No Message.</p>}
     </Page>
   );
 }
